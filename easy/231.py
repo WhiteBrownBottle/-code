8ed15f47416c6ef9685f2b6cf60abf7e8d574058
WhiteBrownBottle <958255724@qq.com>
2020-03-28 23:25:40 +0800

    20200327第21次提交:2题

---
+++ easy/231.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n == 0:
+            return False
+        if n%2 != 0:
+            return False
+        return self.isPowerOfTwo(n / 2)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    output = sol.isPowerOfTwo(512)
+    print(output)
